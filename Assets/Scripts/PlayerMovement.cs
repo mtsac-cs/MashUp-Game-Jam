2a4af509bc91a9d7e9aa9b3f3ba921f30bb6e996
melumilic <56738739+melumilic@users.noreply.github.com>
2021-06-18 12:48:43 -0700

    added more functions to player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,21 +6,30 @@ public class PlayerMovement : ActorMovement
     [NonSerialized]
     public Player player;
     public Rigidbody2D rb;
-    [Range(1,10)]
+    [Range(1, 10)]
     public float moveSpeed = 2.5f;
     Animator animator;
     Vector2 moveDirection = Vector2.down;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        player = GetComponent<Player>();
-        rb = GetComponent<Rigidbody2D>();
+        animator = gameObject.GetOrAddComponent<Animator>();
+        player = gameObject.GetOrAddComponent<Player>();
+        rb = gameObject.GetOrAddComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateMovement();
+    }
+    void CalculateMovement()
+    {
+        if(rb.velocity.magnitude<=.001f){
+            rb.velocity = Vector2.zero;
+        }
+
+        moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        rb.velocity = moveDirection * moveSpeed;
     }
 }
