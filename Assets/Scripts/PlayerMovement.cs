8e62e0eb8dc6b29d367406b2391774ec9f7a9acb
melumilic <56738739+melumilic@users.noreply.github.com>
2021-06-18 12:27:03 -0700

    added some player movement code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,26 @@
 using System;
 using UnityEngine;
+<<<<<<< Updated upstream
 using UnityEngine.Events;
+=======
+using System;
+>>>>>>> Stashed changes
 
 public class PlayerMovement : MonoBehaviour
 {
+    [NonSerialized]
+    public Player player;
+    public Rigidbody2D rb;
+    [Range(1,10)]
+    public float moveSpeed = 2.5f;
+    Animator animator;
+    Vector2 moveDirection = Vector2.down;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        player = GetComponent<Player>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
