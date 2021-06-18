447201cef12a34991c9a9e451d57f83001cb10d8
Thomas <thomaspmar@protonmail.com>
2021-06-18 12:48:04 -0700

    Working on Interaction code

--- Assets/Scripts/ActorMovement.cs
+++ Assets/Scripts/ActorMovement.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class ActorMovement : MonoBehaviour
 {
+    [NonSerialized]
     public UnityEvent onMove;
 
     // Start is called before the first frame update
