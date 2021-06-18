47f4593cc2f589ca546babb68352b17d5ed20376
Thomas <thomaspmar@protonmail.com>
2021-06-18 10:57:41 -0700

    Created PlayerPrefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerMovement))]
 public class Player : MonoBehaviour
 {
     public static Player instance;
+    
+    [NonSerialized]
     public PlayerMovement playerMovement;
 
     void Start()
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
