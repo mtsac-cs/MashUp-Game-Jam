624209161c98be8c891aacbda1b9baa4dc47b95d
melumilic <56738739+melumilic@users.noreply.github.com>
2021-06-20 12:42:34 -0700

    Updated Projectile Script

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -18,6 +18,7 @@ public class Projectile : MonoBehaviour, IInteractable
     float distanceTravelled = 0f;
     float maxProjectileDistance = 500;
 
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -26,19 +27,44 @@ public class Projectile : MonoBehaviour, IInteractable
     public void Init(Vector3 direction)
     {
         //travelDir = Camera.main.ScreenToWorldPoint(new Vector3(direction.x, direction.y)) - Camera.main.WorldToScreenPoint(transform.position);
-        travelDir = (direction - Camera.main.WorldToScreenPoint(transform.position))*1000;
+        travelDir = (direction - Camera.main.WorldToScreenPoint(transform.position));
         travelDir = travelDir.Value.normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Debug.Log(travelDir);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        angle = Vector2.Angle(Vector2.right, travelDir.Value);
+        Debug.Log(angle);
         //float angle = Mathf.Atan2(travelDir.Value.y, travelDir.Value.x) * Mathf.Rad2Deg - 90;
+        // if (travelDir.Value.x > 0 && travelDir.Value.y > 0) //1st quadrant
+        // {
+        //     transform.localRotation = Quaternion.Euler(0, 0, angle);
+        // }
+        // else if (travelDir.Value.x < 0 && travelDir.Value.y > 0) //2nd quadrant
+        // {
+        //     transform.localRotation = Quaternion.Euler(0, 0, angle+90);
+        // }
+        // else if (travelDir.Value.x < 0 && travelDir.Value.y < 0) //3rd quadrant
+        // {
+        //     transform.localRotation = Quaternion.Euler(0, 0, Mathf.Abs(angle)+180);
+        // }
+        // else if (travelDir.Value.x > 0 && travelDir.Value.y < -90) //4th quadrant
+        // {
+        //     transform.localRotation = Quaternion.Euler(0, 0, Mathf.Abs(angle)+270);
+        // }
+        if(angle>0&&travelDir.Value.y>0){
+            transform.localRotation = Quaternion.Euler(0, 0, angle);
+        }else if(travelDir.Value.y<0){
+            Debug.Log("here");
+            transform.localRotation = Quaternion.Euler(0, 0, angle*-1);
+        }
 
         //transform.Rotate(transform.rotation.x, transform.rotation.y, angle);
-        rb.MoveRotation(angle);
-
+        //rb.MoveRotation(angle);
         direction = travelDir.Value;
         Vector2 test = new Vector2(direction.x, direction.y);
-        rb.AddForce(test * projectileSpeed * Time.deltaTime, ForceMode2D.Impulse);
-    }
+        //rb.AddForce(test * projectileSpeed * .2f, ForceMode2D.Impulse);
+        rb.AddForce(travelDir.Value * projectileSpeed * 0.1f, ForceMode2D.Impulse);
 
+    }
 
     private void FixedUpdate()
     {
