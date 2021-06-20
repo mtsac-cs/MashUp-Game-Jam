94dd38f208346de2590caea21590c3a37b270ca0
melumilic <56738739+melumilic@users.noreply.github.com>
2021-06-19 18:34:41 -0700

    added postprocessing and particles

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -18,7 +18,7 @@ public class PlayerMovement : ActorMovement
 
 
     void Start()
-    {
+    { 
         animator = gameObject.GetOrAddComponent<Animator>();
         player = gameObject.GetOrAddComponent<Player>();
         rb = gameObject.GetComponent<Rigidbody2D>();
@@ -35,8 +35,15 @@ public class PlayerMovement : ActorMovement
         if(rb.velocity.magnitude<=.001f){
             rb.velocity = Vector2.zero;
         }
-
+        
         moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")).normalized;
         rb.velocity = moveDirection * moveSpeed;
+
+        if(moveDirection.magnitude>.5f&&rb.velocity.magnitude>1){
+            this.OnMoved(moveDirection,0);
+        }
+    }
+    void TestMove(){
+
     }
 }
--- Assets/Scripts/Player/PlayerSound.cs
+++ Assets/Scripts/Player/PlayerSound.cs
@@ -6,38 +6,35 @@ public class PlayerSound : MonoBehaviour
 {
     GameObject soundContainer;
     [SerializeField]
-    GameObject[] audioClips;
+    PlayerData playerData;
 
     // Start is called before the first frame update
     void Start()
     {
         Player.instance.playerMovement.onMove.AddListener((eventInfo) => PlayMoveSound(eventInfo));
-        soundContainer = Instantiate(new GameObject(),transform.position,Quaternion.identity,transform);
-        
+        soundContainer = Instantiate(new GameObject(), transform.position, Quaternion.identity, transform);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.X)){
-            PlaySound(0);
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            AudioSource.PlayClipAtPoint(playerData.attackSound, transform.position);
         }
-        if(Input.GetKeyDown(KeyCode.C)){
-            PlaySound(1);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            AudioSource.PlayClipAtPoint(playerData.onHitSound, transform.position);
         }
     }
 
-    void PlayMoveSound(ActorMovedEventInfo eventInfo){
-        
+    void PlayMoveSound(ActorMovedEventInfo eventInfo)
+    {
+        AudioSource.PlayClipAtPoint(playerData.walkSound, transform.position);
     }
-    void PlaySound(int index){
-
-    }
-    void PlaySound(AudioClip audioClip){
-        GameObject temp = new GameObject();
-        var audioObject = Instantiate(temp,transform.position,Quaternion.identity,soundContainer.transform);
-        AudioSource audioSource = audioObject.GetOrAddComponent<AudioSource>();
-        audioSource.clip = audioClip;
-        audioSource.Play();
+    void PlaySound()
+    {
+        AudioSource.PlayClipAtPoint(playerData.walkSound, transform.position);
     }
 }
